f5500fa71247b7ef979332d6550ee96c9ad90015
negarbaharmand <negarbaharmand@outlook.com>
2025-06-20 21:36:22 +0200

    Add solution for uppg3: calculate and log the sum of two numbers

--- uppg3.js
+++ uppg3.js
@@ -1,15 +1,12 @@
-
-
 function uppg3() {
-
-    // Skriv ut summan av två tal
-    // deklarera två variabler, a och b
-    // tilldela värdena 5 och 10 till variablerna
-    // skriv ut summan av a och b i konsolen
-
-
-
+  // Skriv ut summan av två tal
+  // deklarera två variabler, a och b
+  // tilldela värdena 5 och 10 till variablerna
+  // skriv ut summan av a och b i konsolen
+  const a = 5;
+  const b = 10;
+  const sum = a + b;
+  console.log(sum);
 }
-
 
 module.exports = { uppg3 };
