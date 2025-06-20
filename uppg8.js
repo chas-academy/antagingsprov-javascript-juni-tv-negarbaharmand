88f3d7ef2d5bc11ad8cb9e2cb85f60047ff72299
negarbaharmand <negarbaharmand@outlook.com>
2025-06-20 21:39:25 +0200

    Add solution for uppg8: filter and log names of people over 30 from an array of objects

--- uppg8.js
+++ uppg8.js
@@ -1,17 +1,30 @@
+function uppg8() {
+  // skapa en array med fem person obkjet
+  // varje person objekt ska ha två properties, name och age
 
+  // skapa en fuktion som tar in en array som argument
+  // skapa ett konditionsvillkor som gör att det bara är namnet på personerna som är
+  //  över 30 år som skrivs ut i konsolen
 
-function uppg8(){
+  // anropa funktionen och skicka med arrayen som argument
 
-// skapa en array med fem person obkjet
-// varje person objekt ska ha två properties, name och age
+  const people = [
+    { name: "Negar", age: 28 },
+    { name: "Sara", age: 32 },
+    { name: "Jonas", age: 35 },
+    { name: "Julia", age: 29 },
+    { name: "Erik", age: 40 },
+  ];
 
-// skapa en fuktion som tar in en array som argument
-// skapa ett konditionsvillkor som gör att det bara är namnet på personerna som är
-//  över 30 år som skrivs ut i konsolen
+  function printOver30(peopleArray) {
+    peopleArray.forEach((person) => {
+      if (person.age > 30) {
+        console.log(person.name);
+      }
+    });
+  }
 
-// anropa funktionen och skicka med arrayen som argument
-
-
+  printOver30(people);
 }
 
 module.exports = { uppg8 };
